2659c47d787de2f6348e2d81c9d0c794cad47d4f
lnr <lnr990920@naver.com>
2021-06-30 20:27:46 +0900

    주석수정

--- client/src/App.js
+++ client/src/App.js
@@ -14,13 +14,7 @@ function App() {
   return (
     <Router>
       <div>
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
+
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/login" component={LoginPage} />
@@ -30,6 +24,5 @@ function App() {
     </Router>
   );
 } 
-
 
 export default App;
